167c793b785a8d33c3c4308a471a64c1a3a5b9d4
akintundem <akintundemayokun@gmail.com>
2024-05-01 19:14:52 -0500

    User Authentication User Registration, User Login, Password Reset

--- Storage/Stub/SQLUserRepository.py
+++
@@ -1,8 +0,0 @@
-class SQLUserRepository(UserRepository):
-    def get_user_by_email(self, email):
-        # Implement SQL query to retrieve user by email
-        pass
-
-    def add_user(self, user):
-        # Implement SQL query to add user to database
-        pass
