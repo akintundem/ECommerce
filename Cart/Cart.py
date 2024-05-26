a8b1cf2065893e828eb8417d7e80d8bd00fa0500
akintundem <akintundemayokun@gmail.com>
2024-05-26 16:01:43 -0500

    Add, remove, update and empty for shopping cart completed

---
+++ Cart/Cart.py
@@ -0,0 +1,212 @@
+import json
+from datetime import datetime
+
+
+class Cart:
+    def __init__(self):
+        pass
+
+    def add_product(self, product_id, quantity, user_id, cursor, conn):
+        if add_to_cart(product_id, quantity, user_id, cursor, conn):
+            return "Added to cart"
+        else:
+            return "Unable to add to cart"
+
+    def remove_product(self, product_id, user_id, cursor, conn):
+        if remove_from_cart(product_id, user_id, cursor, conn):
+            return "Removed from cart"
+        else:
+            return "Unable to remove from cart"
+
+    def update_quantity(self, product_id, new_quantity, user_id, cursor, conn):
+        if update_cart_item_quantity(product_id, new_quantity, user_id, cursor, conn):
+            return "Quantity updated"
+        else:
+            return "Unable to update quantity"
+
+    def empty_cart(self, user_id, cursor, conn):
+        if empty_cart(user_id, cursor, conn):
+            return "Cart emptied"
+        else:
+            return "Unable to empty cart"
+    
+    def view_cart(self, user_id, cursor):
+        cart_items = view_cart(user_id, cursor)
+        if cart_items:
+            return json.dumps(cart_items)
+        else:
+            return "Cart is empty"
+
+def view_cart(user_id, cursor):
+    try:
+        # Retrieve the cart_id associated with the user
+        cursor.execute("SELECT cart_id FROM UserCart WHERE user_id = ?", (user_id,))
+        cart_id = cursor.fetchone()
+
+        if cart_id is None:
+            # User doesn't have a cart, return an empty list
+            return []
+
+        cart_id = cart_id[0]  # Extract cart_id from the result
+
+        # Retrieve all products in the user's cart
+        cursor.execute("SELECT product_id, quantity FROM ShoppingCart WHERE cart_id = ?", (cart_id,))
+        cart_items = cursor.fetchall()
+
+        products = []
+        for item in cart_items:
+            product_id, quantity = item
+            # Fetch product details based on product_id
+            cursor.execute("SELECT * FROM Products WHERE product_id = ?", (product_id,))
+            product_details = cursor.fetchone()
+            if product_details:
+                product = {
+                    'product_id': product_id,
+                    'product_name': product_details[1],
+                    'discounted_price': float(product_details[2]),
+                    'actual_price': float(product_details[3]),
+                    'discount_percentage': float(product_details[4]),
+                    'rating': float(product_details[5]),
+                    'rating_count': int(product_details[6]),
+                    'about_product': product_details[7],
+                    'number_of_inventory': int(product_details[8]),
+                    'img_link': product_details[9],
+                    'quantity': quantity
+                }
+                products.append(product)
+
+        return products
+    except Exception as e:
+        # Handle any errors
+        print("Error:", e)
+        return []
+
+
+
+def add_to_cart(product_id, quantity, user_id, cursor, conn):
+    try:
+        # Begin a transaction
+        cursor.execute("BEGIN TRANSACTION")
+        
+        created_at = datetime.now()
+
+        # Execute INSERT query
+        query_insert = '''INSERT INTO Users (username, password, email, full_name, address, phone_number, created_at)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                    '''
+        cursor.execute(query_insert, ("akintundemayokun@gmail.com", "Iamayokakfwesh1", "akintundemayokun@gmail.com", "Mayokun Moses Akintunde", "63 Lark Ridge", "080213232", created_at))
+
+        # Check if the user has a cart
+        cursor.execute("SELECT cart_id FROM UserCart WHERE user_id = ?", (user_id,))
+        cart_id = cursor.fetchone()
+        
+        if cart_id is None:
+            # If the user doesn't have a cart, create a new cart entry
+            cursor.execute(
+                "INSERT INTO UserCart (user_id) OUTPUT INSERTED.cart_id VALUES (?)",
+                (user_id,)
+            )
+            cart_id = cursor.fetchone()[0]    
+        else:
+            cart_id = cart_id[0]  # Extract cart_id from the result
+        
+        # Insert the item into the shopping cart
+        cursor.execute("INSERT INTO ShoppingCart (cart_id, product_id, quantity) VALUES (?, ?, ?)", (cart_id, product_id, quantity))
+        
+        # Commit the transaction
+        conn.commit()
+        
+        return True
+    except Exception as e:
+        print("Error:", e)
+        conn.rollback()  # Rollback the transaction in case of an error
+        return False
+    
+def remove_from_cart(product_id, user_id, cursor, conn):
+    try:
+        # Begin a transaction
+        cursor.execute("BEGIN TRANSACTION")
+
+        # Get the cart_id associated with the user
+        cursor.execute("SELECT cart_id FROM UserCart WHERE user_id = ?", (user_id,))
+        cart_id = cursor.fetchone()
+
+        if cart_id is None:
+            # User doesn't have a cart, no action needed
+            return False
+
+        cart_id = cart_id[0]  # Extract cart_id from the result
+
+        # Remove the item from the shopping cart
+        cursor.execute("DELETE FROM ShoppingCart WHERE cart_id = ? AND product_id = ?", (cart_id, product_id))
+
+        # Check if the user has any remaining items in the cart
+        cursor.execute("SELECT COUNT(*) FROM ShoppingCart WHERE cart_id = ?", (cart_id,))
+        remaining_items_count = cursor.fetchone()[0]
+
+        if remaining_items_count == 0:
+            # If the user has no remaining items, delete the cart entry from UserCart
+            cursor.execute("DELETE FROM UserCart WHERE cart_id = ?", (cart_id,))
+
+        # Commit the transaction
+        conn.commit()
+
+        return True
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction in case of an error
+        return False
+
+def update_cart_item_quantity(product_id, new_quantity, user_id, cursor, conn):
+    try:
+        # Begin a transaction
+        cursor.execute("BEGIN TRANSACTION")
+
+        # Get the cart_id associated with the user
+        cursor.execute("SELECT cart_id FROM UserCart WHERE user_id = ?", (user_id,))
+        cart_id = cursor.fetchone()
+
+        if cart_id is None:
+            # User doesn't have a cart, no action needed
+            return False
+
+        cart_id = cart_id[0]  # Extract cart_id from the result
+
+        # Update the quantity of the item in the shopping cart
+        cursor.execute("UPDATE ShoppingCart SET quantity = ? WHERE cart_id = ? AND product_id = ?", (new_quantity, cart_id, product_id))
+
+        # Commit the transaction
+        conn.commit()
+
+        return True
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction in case of an error
+        return False
+
+def empty_cart(user_id, cursor, conn):
+    try:
+        # Begin a transaction
+        cursor.execute("BEGIN TRANSACTION")
+
+        # Get the cart_id associated with the user
+        cursor.execute("SELECT cart_id FROM UserCart WHERE user_id = ?", (user_id,))
+        cart_id = cursor.fetchone()
+
+        if cart_id is None:
+            # User doesn't have a cart, no action needed
+            return False
+
+        cart_id = cart_id[0]  # Extract cart_id from the result
+
+        # Delete all items from the shopping cart associated with the user's cart
+        cursor.execute("DELETE FROM ShoppingCart WHERE cart_id = ?", (cart_id,))
+
+        # Remove the cart entry from the UserCart table
+        cursor.execute("DELETE FROM UserCart WHERE cart_id = ?", (cart_id,))
+
+        # Commit the transaction
+        conn.commit()
+
+        return True
+    except Exception as e:
+        conn.rollback()  # Rollback the transaction in case of an error
+        return False
--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ import json
 from VerifyToken import TokenVerifier
 from Recommendation.Recommend import Recommendation
 from Search.SearchEngine import Search
-from Cart import Cart
+from Cart.Cart import Cart
 from Pay import PaymentHandler
 from flask import Flask, request
 from db.db import DatabaseManager
@@ -15,6 +15,7 @@ VerifyUser = TokenVerifier("S", "A")
 config_file = 'db/db_cred.json' 
 datab = DatabaseManager(config_file)
 db = datab.get_cursor()
+conn = datab.get_connection()
 
 recommendation = Recommendation()
 search = Search()
@@ -35,7 +36,7 @@ def recommend():
         response = "Invalid token"
         return json.dumps({'message': response})
 
-@app.route('/search', methods=['GET'])
+@app.route('/search', methods=['POST'])
 def searchProd():
     token = request.headers.get('Token')
     params = request.args.get('search')
@@ -49,6 +50,69 @@ def searchProd():
     else:
         response = "Invalid token"
         return json.dumps({'message': response})
+@app.route('/cart', methods=['GET'])
+def cartView():
+    token = request.headers.get('Token')
+    if VerifyUser.verify_tokens(token):
+        userID = 1
+        response = cart.view_cart(userID,db)
+        if response:
+            return json.dumps({'message': response})
+        else:
+            response = "None"
+            return json.dumps({'message': response})
+    else:
+        response = "Invalid token"
+        return json.dumps({'message': response})
+    
+@app.route('/cart', methods=['POST'])
+def cartActions():
+    token = request.headers.get('Token')
+    data = json.loads(request.data.decode('utf-8'))
+    
+    if VerifyUser.verify_tokens(token):
+        action = data.get('action')
+        userID = 1
+        # Handle different actions
+        # Add: Add a product to the cart{
+        # "action": "add",
+        # "product_id": "B097JVLW3L",
+        # "quantity": 1
+        # }
+        if action == 'add':
+            product_id = data.get('product_id')
+            quantity = data.get('quantity')
+            response = cart.add_product(product_id, quantity, 1, db, conn)
+        # Remove: Remove a product from the cart{
+        # "action": "remove",
+        # "product_id": "123"
+        # }
+        elif action == 'remove':
+            product_id = data.get('product_id')
+            response = cart.remove_product(product_id, 1, db, conn)
+        # Update Quantity: Update the quantity of a product in the cart{
+        # "action": "update_quantity",
+        # "product_id": "123",
+        # "quantity": 2
+        # }
+        elif action == 'update_quantity':
+            product_id = data.get('product_id')
+            quantity = data.get('quantity')
+            response = cart.update_quantity(product_id, quantity, userID, db, conn)
+        # empty: Empty the cart{
+        #   "action": "empty"
+        # }
+        elif action == 'empty':
+            response = cart.empty_cart(userID, db, conn)
+        else:
+            response = "Invalid action"
+        
+        # Return response
+        return json.dumps({'message': response})
+    else:
+        response = "Invalid token"
+        return json.dumps({'message': response})
+
 
 @app.route('/product_detail', methods=['GET'])
 def product():
@@ -66,23 +130,7 @@ def product():
         response = "Invalid token"
         return json.dumps({'message': response})
         
-@app.route('/save_to_cart', methods=['POST'])
-def saveCart():
-    data = json.loads(request.data)
-    token = data.get('Token')
-    if VerifyUser.verify_tokens(token):
-        request_message = data.get('message')
-        response = cart.add_product(request_message,db) 
-        if response:
-            return json.dumps({'message': response})
-        else:
-            response = "None"
-            return json.dumps({'message': response})
-    else:
-        response = "Invalid token"
-        return json.dumps({'message': response})
-
-
+            
 @app.route('/delete_from_cart', methods=['POST'])
 def deleteCart():
     data = json.loads(request.data)
